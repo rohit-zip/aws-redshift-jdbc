79417363384241760287ff10b36759113710c784
Rohit <rohitparih@gmail.com>
2024-05-16 13:33:38 +0530

    Fixes

--- src/main/java/com/redshift/Main.java
+++ src/main/java/com/redshift/Main.java
@@ -2,6 +2,11 @@ package com.redshift;
 
 import lombok.SneakyThrows;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Properties;
+import java.util.function.BiConsumer;
+
 /**
  * Owner - Rohit Parihar
  * Author - rohit
@@ -17,8 +22,12 @@ public class Main {
     public static void main(String[] args) {
 
         RedshiftDriver.loadRedshiftDriver();
-        RedshiftConnection.getConnection("admin", "@@@@@@");
+        Connection connection = RedshiftConnection.getConnection("admin", "Redshift1234");
         System.out.println("Connection Established");
-
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM public.dummy");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1)); // New line after printing each row
+            System.out.println(resultSet.getString(2)); // New line after printing each row
+        }
     }
 }
